fb4331f0c404a5f14829628ca8afe81235e77b11
phyng <phyngk@gmail.com>
2018-08-01 13:41:22 +0800

    feat(dwapp): add dwapp build script

---
+++ build_dwapp.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+
+from __future__ import unicode_literals, absolute_import, print_function, division
+
+import os
+import shutil
+
+DIR_BASE = os.path.dirname(__file__)
+DIR_DIST = os.path.join(DIR_BASE, 'dist')
+DIR_PLUGINS = os.path.abspath('../bazaar4/dwapp/defaultapp/pages')
+
+
+def get_plugin_name(name):
+    return 'plugin-comz' + name.replace('-', '')
+
+
+def convert_package():
+    packages = [i for i in os.listdir(DIR_DIST) if os.path.isdir(os.path.join(DIR_DIST, i))]
+    for package in packages:
+        plugin_name = get_plugin_name(package)
+        print(f'{package} -> {plugin_name}')
+        source_dir = os.path.join(DIR_DIST, package)
+        target_dir = os.path.join(DIR_PLUGINS, plugin_name)
+        if os.path.exists(target_dir):
+            shutil.rmtree(target_dir)
+        shutil.copytree(source_dir, target_dir)
+
+
+def main():
+    convert_package()
+
+
+if __name__ == '__main__':
+    main()
